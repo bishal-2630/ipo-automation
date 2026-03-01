1c6c3ec0a7327b39a3a179bd76aaca6e16459d47
Bishal <kbishal177@gmail.com>
2026-03-01 21:46:27 +0545

    Switched to Neon PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -82,15 +82,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
+import os
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Use DATABASE_URL environment variable if it exists (for Neon/PostgreSQL)
+# Fallback to local SQLite for development
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        ssl_require=True if os.environ.get('DATABASE_URL') else False
+    )
 }
+
+# If we are using local SQLite, we don't need ssl_require
+if not os.environ.get('DATABASE_URL'):
+    DATABASES['default']['OPTIONS'] = {}
 
 
 # Password validation
