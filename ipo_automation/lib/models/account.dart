842665b1977eeac808180d5a9deec59b32a5ad4f
Bishal <kbishal177@gmail.com>
2026-03-01 19:22:26 +0545

    Added email field to Add Account screen and updated Flutter model

--- ipo_automation/lib/models/account.dart
+++ ipo_automation/lib/models/account.dart
@@ -3,9 +3,17 @@ class Account {
   final String user;
   final String bank;
   final String lastApplied;
+  final String? email;
   final bool isActive;
 
-  Account({required this.id, required this.user, required this.bank, required this.lastApplied, required this.isActive});
+  Account({
+    required this.id,
+    required this.user,
+    required this.bank,
+    required this.lastApplied,
+    this.email,
+    required this.isActive,
+  });
 
   factory Account.fromJson(Map<String, dynamic> json) {
     return Account(
@@ -13,6 +21,7 @@ class Account {
       user: json['meroshare_user'],
       bank: json['bank_name'],
       lastApplied: json['last_applied'] ?? 'Never',
+      email: json['email'],
       isActive: json['is_active'],
     );
   }
