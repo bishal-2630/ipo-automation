17ffc269ddc9a5524a23db1791d98f9fd0b112b4
Bishal <kbishal177@gmail.com>
2026-03-02 20:53:18 +0545

    Migrate to Vercel deployment and cleanup legacy Hugging Face files

---
+++ vercel_wsgi.py
@@ -0,0 +1,8 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Set the settings module for the Vercel serverless environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# Get the WSGI application
+app = get_wsgi_application()
