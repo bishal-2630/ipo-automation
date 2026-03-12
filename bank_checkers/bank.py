f4aeb4e08e9adae175e97cf608e5e6258d01b501
Bishal <kbishal177@gmail.com>
2026-03-12 21:20:40 +0545

    Fixed bank automation

--- bank_checkers/bank.py
+++ bank_checkers/bank.py
@@ -15,9 +15,9 @@ BANK_CONFIGS: dict[str, dict] = {
     "nic_asia": {
         "name": "NIC Asia Bank",
         "url": "https://omni.nicasiabank.com/sign-in",
-        "user_sel": "input[placeholder*='Mobile Number']",
+        "user_sel": "input[placeholder*='Mobile'], input[placeholder*='mobile'], input[name*='mobile' i], input[id*='mobile' i], input[formcontrolname*='mobile' i], input[type='text']",
         "pass_sel": "input[type='password']",
-        "submit_sel": "button:has-text('Log In')",
+        "submit_sel": "button:has-text('Log In'), button:has-text('Login'), button:has-text('Sign In'), button[type='submit']",
         "balance_sel": ".balance, [class*='balance'], [id*='balance']",
     },
     "nabil": {
