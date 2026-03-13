9856f683e10c3d4fda229a3260f19d4904d8d676
Bishal <kbishal177@gmail.com>
2026-03-13 14:26:35 +0545

    chore: remove Dockerfile and configure native Render deployment

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from automation.views import home_view, HealthView
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('health/', HealthView.as_view(), name='health'),
     path('admin/', admin.site.urls),
     path('api/', include('automation.urls')),
